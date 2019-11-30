98b52627e11c6df9c26c86d3cee0642491c0c315
Anchiqua <anja.leskovsek@gmail.com>
2019-11-30 19:42:56 +0100

    split

---
+++ domaca-naloga-01/lambda/split.ml
@@ -0,0 +1,14 @@
+﻿let split sez =
+      let reverse sez =
+      let rec reverse_aux acc = function
+            | [] -> acc
+            | x :: xs -> reverse_aux (x :: acc) xs          
+   in reverse_aux [] sez
+      in  
+    let rec split_aux sez a b =
+            match sez with
+            |[] -> (reverse a, reverse b)
+            |(x, y)::xs -> split_aux xs (x::a) (y::b)  
+        in split_aux sez [] []        
+
+in split [(1, 2);(3, 4);(5, 6)];;
