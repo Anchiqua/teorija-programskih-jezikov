9e4aefce77cc80cbc226cfd4d24051afd04fb983
Anchiqua <anja.leskovsek@gmail.com>
2019-12-01 10:21:12 +0100

    dodala kodo

--- domaca-naloga-01/lambda/lazyEval.ml
+++ domaca-naloga-01/lambda/lazyEval.ml
@@ -43,18 +43,40 @@ let rec eval_exp = function
       | S.RecLambda (f, x, e) as rec_f -> eval_exp (S.subst [(f, rec_f); (x, v)] e)
       | _ -> failwith "Function expected"
       end
+  | S.Pair (e1, e2) ->
+      let v1 = eval_exp e1
+      and v2 = eval_exp e2
+      in S.Pair (v1, v2)
+  | S.Fst e ->
+      begin match eval_exp e with
+      | S.Pair (e1, _) -> eval_exp e1
+      | _ -> failwith "Pair expected"
+      end
+  | S.Snd e ->
+      begin match eval_exp e with
+      | S.Pair (_, e2) -> eval_exp e2
+      | _ -> failwith "Pair expected"
+      end
+  | S.Match (e, e1, x, xs, e2) ->
+      begin match eval_exp e with
+      | S.Nil -> eval_exp e1
+      | S.Cons (y, ys) -> eval_exp (S.subst [(x, y); (xs, ys)] e2)
+      | _ -> failwith "List expected"
+      end
+   
 and eval_int e =
   match eval_exp e with
   | S.Int n -> n
   | _ -> failwith "Integer expected"
 
 let is_value = function
-  | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> true
+  | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ | S.Pair (v1, v2)
+  | S.Cons (v1, v2) -> true
   | S.Var _ | S.Plus _ | S.Minus _ | S.Times _ | S.Equal _ | S.Less _ | S.Greater _
   | S.IfThenElse _ | S.Apply _ -> false
 
 let rec step = function
-  | S.Var _ | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ -> failwith "Expected a non-terminal expression"
+  | S.Var _ | S.Int _ | S.Bool _ | S.Lambda _ | S.RecLambda _ | S.Pair -> failwith "Expected a non-terminal expression"
   | S.Plus (S.Int n1, S.Int n2) -> S.Int (n1 + n2)
   | S.Plus (S.Int n1, e2) -> S.Plus (S.Int n1, step e2)
   | S.Plus (e1, e2) -> S.Plus (step e1, e2)
@@ -79,6 +101,15 @@ let rec step = function
   | S.Apply (S.RecLambda (f, x, e) as rec_f, v) when is_value v -> S.subst [(f, rec_f); (x, v)] e
   | S.Apply ((S.Lambda _ | S.RecLambda _) as f, e) -> S.Apply (f, step e)
   | S.Apply (e1, e2) -> S.Apply (step e1, e2)
+  | S.Fst (S.Pair (e1, e2)) -> e1
+  | S.Fst e -> S.Fst(step e)
+  | S.Snd (S.Pair (e1, e2)) -> e2
+  | S.Snd e -> S.Snd(step e)
+  | S.Nil -> S.Nil
+  | S.Match (S.Nil, e1, x, xs, e2) -> e1
+  | S.Match (S.Cons (v1, v2) as e, e1, x, xs, e2) when is_value e -> (S.subst [(x, v1); (xs, v2)] e2)
+  | S.Match (v, e1, x, xs, e2) when is_value v -> failwith "Pricakujem seznam"
+  | S.Match (e, e1, x, xs, e2) -> S.Match (step e, e1, x, xs, e2)
 
 let big_step e =
   let v = eval_exp e in
